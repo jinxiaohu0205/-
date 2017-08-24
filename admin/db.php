e76ce2da2de99d0cc68a6a6878b27b69410ebbb2
jinxiaohu0205 <709620261@qq.com>
2017-08-24 18:44:54 +0800

    d

--- admin/db.php
+++
@@ -1,4 +0,0 @@
-<?php
- $db=new mysqli("localhost","root","","user");
- $db->query("set names utf8");
-?>
--- 脑洞2/js/public.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-	//	适配
-	function size(originwidth){
-    	var width=document.documentElement.clientWidth;
-    	var bili=width/originwidth*100+"px";
-    	$("html").css("font-size",bili);
-    }
-	 size(640);
-}
